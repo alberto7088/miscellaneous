31ae1f05d93b6d4dc3ed457e075ce1376a1cf129
david-macleod <macleod.david@live.co.uk>
2018-03-28 12:51:55 +0100

    Tabs to spaces

--- dm/sorting_algorithms.py
+++ dm/sorting_algorithms.py
@@ -2,11 +2,11 @@ def insert_sort(unsorted):
     sorted = [] 
     while unsorted:
         x = unsorted.pop()
-		len_sorted = len(sorted) 
-		for i in range(len_sorted):
-			if sorted[i] <= x:
-				sorted.insert(i, x)
-				break 
-		if len_sorted == len(sorted):
-			sorted.append(x)
-	return sorted 
+	len_sorted = len(sorted) 
+        for i in range(len_sorted):
+            if sorted[i] <= x:
+                sorted.insert(i, x)
+                break 
+        if len_sorted == len(sorted):
+            sorted.append(x)
+     return sorted 
